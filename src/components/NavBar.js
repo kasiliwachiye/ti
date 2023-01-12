c69c7f52bf8968a999aaeb0cdd8dfe6fd51a5275
kasiliwachiye <63935096+kasiliwachiye@users.noreply.github.com>
2023-01-12 22:12:14 +0300

    popup for adding user. store setup

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -7,7 +7,72 @@ const NavBar = () => {
         <h1 className=" normal-case text-xl">Touch Inspo</h1>
       </div>
       <div className="navbar-end">
-        <button className="btn btn-outline">Add User</button>
+        <div>
+          {/* Edit button to open modal */}
+          <label
+            htmlFor="my-modal-4"
+            className="btn btn-outline"
+          >
+            Add user
+          </label>
+          <input type="checkbox" id="my-modal-4" className="modal-toggle" />
+          <label htmlFor="my-modal-4" className="modal cursor-pointer">
+            <label
+              className="modal-box relative flex justify-center"
+              htmlFor=""
+            >
+              <div>
+                <div className="form-control w-full max-w-xs">
+                  <label className="label">
+                    <span className="label-text">Name</span>
+                  </label>
+                  <input
+                    type="text"
+                    placeholder="James Bond"
+                    className="input input-bordered w-full max-w-xs"
+                  />
+                </div>
+                <div className="form-control w-full max-w-xs">
+                  <label className="label">
+                    <span className="label-text">Email</span>
+                  </label>
+                  <input
+                    type="email"
+                    placeholder="jamesbond@mail.com"
+                    className="input input-bordered w-full max-w-xs"
+                  />
+                </div>
+                <div className="form-control w-full max-w-xs">
+                  <label className="label">
+                    <span className="label-text">Occupation</span>
+                  </label>
+                  <input
+                    type="text"
+                    placeholder="Secret service agent"
+                    className="input input-bordered w-full max-w-xs"
+                  />
+                </div>
+                <div className="form-control w-full max-w-xs">
+                  <label className="label">
+                    <span className="label-text">Bio</span>
+                  </label>
+                  <textarea
+                    className="textarea textarea-bordered"
+                    placeholder="The name's Bond. James Bond."
+                  ></textarea>
+                  <div className="modal-action">
+                    <label
+                      htmlFor="my-modal-4"
+                      className="btn btn-outline btn-primary w-full"
+                    >
+                      Edit
+                    </label>
+                  </div>
+                </div>
+              </div>
+            </label>
+          </label>
+        </div>
       </div>
     </div>
   );
